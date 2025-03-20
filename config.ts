64243fe439426c7b93c110a3320ba68005a838e3
Elihuso Quigley <Elihuso@outlook.com>
2025-03-20 11:28:35 +0800

    [U] port gnome 48

--- config.ts
+++ config.ts
@@ -10,7 +10,13 @@ export const an = [
     {name: '紫茗纪日', date: (new Date(2024, 6, 5, 0, 0, 0))},
     {name: '风和纪日', date: (new Date(2024, 7, 4, 0, 0, 0))},
     {name: '琼依纪日', date: (new Date(2024, 8, 18, 0, 0, 0))},
-    {name: '潮帆轻语', date: (new Date(2024, 9, 24, 0, 0, 0))}
+    {name: '潮帆轻语', date: (new Date(2024, 9, 3, 0, 0, 0))},
+    {name: '明堂纪日', date: (new Date(2024, 10, 5, 0, 0, 0))},
+    {name: '域外飞雪', date: (new Date(2024, 10, 24, 0, 0, 0))},
+    {name: '浮世繪夢', date: (new Date(2024, 11, 22, 0, 0, 0))},
+    {name: '白雲飄絮', date: (new Date(2025, 1, 6, 0, 0, 0))},
+    {name: '無夢殘願', date: (new Date(2025, 1, 20, 0, 0, 0))},
+    {name: '海崖冬淚', date: (new Date(2025, 2, 17, 12, 0, 0))}
 ].sort((a, b) => {
     return a.date.getTime() - b.date.getTime()
 }).reverse() as An[]
